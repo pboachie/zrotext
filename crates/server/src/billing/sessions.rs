2a1dafe7010b50d948d23b44b1d41ccf5511f28f
pboachie <princeboachie@gmail.com>
2026-09-23 18:09:01 -0700

    test: apply current migrations in Stripe Checkout smoke
    Signed-off-by: pboachie <princeboachie@gmail.com>

--- crates/server/src/billing/sessions.rs
+++ crates/server/src/billing/sessions.rs
@@ -881,7 +881,11 @@ mod tests {
             include_str!("../../../../deploy/compose/migrations/012_auth_abuse_limits.sql"),
             include_str!("../../../../deploy/compose/migrations/013_owner_mfa.sql"),
             include_str!("../../../../deploy/compose/migrations/014_owner_mfa_failure_budget.sql"),
+            include_str!("../../../../deploy/compose/migrations/015_webhook_kek_commitments.sql"),
             include_str!("../../../../deploy/compose/migrations/016_auth_abuse_atomic.sql"),
+            include_str!("../../../../deploy/compose/migrations/017_billing_device_caps.sql"),
+            include_str!("../../../../deploy/compose/migrations/018_sealed_inbound_identity.sql"),
+            include_str!("../../../../deploy/compose/migrations/019_line_activation_contract.sql"),
         ] {
             db.batch_execute(sql).await.unwrap();
         }
